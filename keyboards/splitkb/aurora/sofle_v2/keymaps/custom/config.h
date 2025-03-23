d6a4b39f768439515ca6fc6d25ebdf08931e54a8
Tuomas Vaherkoski <tuomasvaherkoski@gmail.com>
2025-03-23 15:38:12 +0200

    customize smtd release timeout for shift keys

--- keyboards/splitkb/aurora/sofle_v2/keymaps/custom/config.h
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/custom/config.h
@@ -6,8 +6,9 @@
 
 //#define SMTD_DEBUG_ENABLED
 #define MAX_DEFERRED_EXECUTORS 10 // sm_td
-#define TAPPING_TERM 250
+#define TAPPING_TERM 220
 
 // This is the time in ms to consider two keys released within that period as a hold action for the first key and a tap action for second.
 // If two keys has bigger time between their releases, they will be considered as a tap action for both keys.
+// NOTE: there can be  OVERRIDDES in keymap.c get_smtd_timeout
 #define SMTD_GLOBAL_RELEASE_TERM 30 // avoid E>N to become command N
