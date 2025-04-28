44eee893c20397a12918d2e265826d9c8cfa9e7e
Tuomas Vaherkoski <tuomasvaherkoski@gmail.com>
2025-04-28 20:53:15 +0300

    chocofi sm_td

--- keyboards/tuopppi/chocofi/keymaps/default/keymap.c
+++ keyboards/tuopppi/chocofi/keymaps/default/keymap.c
@@ -1,11 +1,37 @@
 #include QMK_KEYBOARD_H
 
+#include "keycodes.h"
+#include "quantum_keycodes.h"
+#include "action.h"
+#include "process_combo.h"
+#include "process_key_override.h"
+
+enum layers {
+    DEFAULT = 0,
+    SYM = 1,
+    NUM = 2,
+    MOUSE = 3
+};
+
+enum custom_keycodes {
+    CKC_A = SAFE_RANGE,
+    CKC_S,
+    CKC_T,
+    CKC_V,
+    CKC_N,
+    CKC_E,
+    CKC_L,
+    CKC_P,
+    CKC_BSPC,
+};
+
 enum unicode_names {
     ae, // ä
     AE, // Ä
     oe, // ö
     OE, // Ö
 };
+
 
 const uint32_t PROGMEM unicode_map[] = {
     [ae] = 0x00E4,
@@ -14,26 +40,144 @@ const uint32_t PROGMEM unicode_map[] = {
     [OE] = 0x00D6,
 };
 
+#define KC_QUIT (QK_LCTL | QK_LGUI | KC_Q)
 #define KC_OE  UP(oe, OE)
 #define KC_AE  UP(ae, AE)
-
+#define KC_SCREENSHOT LSG(KC_4)
+#define KC_SCREENCAP  LSG(KC_5)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
+  [DEFAULT] = LAYOUT(
   //+--------------------------------------------+                    +---------------------------------------------+
       KC_ESC,  KC_W,    KC_D,    KC_R,    KC_K,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
-      KC_Q,    KC_S,    KC_T,    KC_V,    KC_G,                         KC_H,    KC_N,    KC_E,    KC_L,    KC_OE,
+      KC_Q,    CKC_S,   CKC_T,   CKC_V,   KC_G,                         KC_H,    CKC_N,   CKC_E,   CKC_L,   KC_OE,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
-      KC_A,    KC_X,    KC_C,    KC_F,    KC_B,                         KC_J,    KC_M,    KC_COMM, KC_DOT,  KC_P, 
+      CKC_A,   KC_X,    KC_C,    KC_F,    KC_B,                         KC_J,    KC_M,    KC_COMM, KC_DOT,  CKC_P, 
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
-      KC_Z,                      KC_LSFT, KC_AE, XXXXXXX,   KC_SPACE, KC_BSPC, XXXXXXX,                   KC_RSFT
+      KC_Z,                      OSL(NUM), KC_AE,  OSL(SYM),   KC_SPACE, CKC_BSPC, OSL(MOUSE),            KC_QUOTE
+  ),
+  [SYM] = LAYOUT(
+  //+--------------------------------------------+                    +---------------------------------------------+
+      KC_NO,   KC_LT    , KC_GT, KC_GRAVE, KC_NO,                       KC_AMPERSAND, KC_SEMICOLON, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_NO,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_CIRCUMFLEX, CW_TOGG, KC_PLUS, KC_EQUAL, KC_HASH,               KC_PIPE, KC_COLON, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_QUESTION,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_EXCLAIM, KC_SLASH, KC_ASTERISK, KC_BACKSLASH, KC_NO,        KC_TILDE, KC_DOLLAR, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_AT,
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
+      KC_NO,                     KC_TRNS, KC_TRNS, KC_TRNS,    G(KC_Z), SGUI(KC_Z), KC_TRNS,               KC_PERCENT
+  ),
+  [NUM] = LAYOUT(
+  //+--------------------------------------------+                    +---------------------------------------------+
+      KC_NO,   KC_F1,   KC_UP,   KC_F2,   KC_NO,                        KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,                      KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_NO,   G(KC_X), G(KC_C), G(KC_V), KC_NO,                        KC_0,    KC_1,    KC_2,    KC_3,    KC_NO, 
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
+      KC_NO,                     KC_NO,   KC_NO,   KC_NO,      CKC_BSPC, KC_DEL, KC_NO,                     KC_NO
+  ),
+  [MOUSE] = LAYOUT(
+  //+--------------------------------------------+                    +---------------------------------------------+
+      KC_NO,   KC_NO,   KC_NO,   KC_SCREENCAP, KC_SCREENSHOT,           MS_BTN3, MS_BTN1, MS_UP  , MS_BTN2, KC_NO,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   MS_LEFT, MS_DOWN, MS_RGHT, KC_NO,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+---------|
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+---------|
+      KC_QUIT,                   KC_NO,   KC_NO,  KC_NO,       KC_NO,   KC_NO,   KC_NO,                     QK_BOOT
   )
 };
 
-const uint16_t PROGMEM combo_enter[] = {KC_E, KC_L, COMBO_END};
+#include "sm_td.h"
+
+// COMBOS
+const uint16_t PROGMEM combo_enter[] = {CKC_E, CKC_L, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_COMMA, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_layer_mouse[] = {OSL(SYM), KC_SPACE, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo_enter, KC_ENTER),
     COMBO(combo_tab, KC_TAB),
+    COMBO(combo_layer_mouse , MO(MOUSE)),
 };
+
+// KEY OVERRIDES
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t comma_minus = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, KC_MINUS);
+const key_override_t dot_underscore = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_UNDERSCORE);
+const key_override_t f1_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_1, KC_F1);
+const key_override_t f2_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_F2);
+const key_override_t f3_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_3, KC_F3);
+const key_override_t f4_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_F4);
+const key_override_t f5_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_5, KC_F5);
+const key_override_t f6_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_6, KC_F6);
+const key_override_t f7_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_7, KC_F7);
+const key_override_t f8_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_8, KC_F8);
+const key_override_t f9_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_9, KC_F9);
+const key_override_t *key_overrides[] = {
+	&delete_key_override,
+    &comma_minus,
+    &dot_underscore,
+    &f1_key_override,
+    &f2_key_override,
+    &f3_key_override,
+    &f4_key_override,
+    &f5_key_override,
+    &f6_key_override,
+    &f7_key_override,
+    &f8_key_override,
+    &f9_key_override,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_smtd(keycode, record)) {
+        return false;
+    }
+
+    return true; // Process all other keycodes normally
+}
+
+// https://github.com/stasmarkin/sm_td/blob/main/docs/070_customization_timeouts.md
+// defaults are set in config.h
+uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
+    switch (keycode) {
+        case CKC_V:
+        case CKC_N:
+            // all but left and right shift SM_TD keys get short release
+            // timeout to avoid accidental triggering of tap dance keys
+            if (SMTD_TIMEOUT_RELEASE) return 90;
+    }
+
+    return get_smtd_timeout_default(timeout);
+}
+
+#define SMTD_TD_HOLD_ON_MKEY(macro_key, tap_key, hold_key, threshold, use_cl) \
+    SMTD_DANCE(macro_key,                                        \
+        NOTHING,                                                 \
+        SMTD_TAP_16(use_cl, tap_key),                            \
+        SMTD_LIMIT(threshold,                                    \
+            SMTD_TAP_16(use_cl, hold_key),                       \
+            SMTD_REGISTER_16(use_cl, tap_key)),                  \
+        SMTD_LIMIT(threshold,                                    \
+            SMTD_UNREGISTER_16(use_cl, hold_key),                \
+            SMTD_UNREGISTER_16(use_cl, tap_key))                 \
+    )
+
+smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+    switch (keycode) {
+        // home row mods
+        SMTD_MTE_ON_MKEY(CKC_A, KC_A, KC_LEFT_CTRL)
+        SMTD_MTE_ON_MKEY(CKC_S, KC_S, KC_LEFT_ALT)
+        SMTD_MTE_ON_MKEY(CKC_T, KC_T, KC_LEFT_GUI)
+        SMTD_MTE_ON_MKEY(CKC_V, KC_V, KC_LSFT)
+        SMTD_MTE_ON_MKEY(CKC_N, KC_N, KC_RSFT)
+        SMTD_MTE_ON_MKEY(CKC_E, KC_E, KC_RIGHT_GUI)
+        SMTD_MTE_ON_MKEY(CKC_L, KC_L, KC_RIGHT_ALT)
+        SMTD_MTE_ON_MKEY(CKC_P, KC_P, KC_RIGHT_CTRL)
+
+        // tap dance
+        SMTD_TD_HOLD_ON_MKEY(CKC_BSPC, KC_BSPC, A(KC_BSPC), 2, true)
+    }
+
+    return SMTD_RESOLUTION_UNHANDLED;
+}
